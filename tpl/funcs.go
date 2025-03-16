5168d1f35627672940d6461a4211455c2b65d324
Brandon Epperson <brandon.epperson@protonmail.com>
2025-03-15 20:12:04 -0400

    add: mul template function

--- tpl/funcs.go
+++ tpl/funcs.go
@@ -8,4 +8,5 @@ var funcs = template.FuncMap{
 	// arithmetic
 	"add": func(a, b int) int { return a + b },
 	"sub": func(a, b int) int { return a - b },
+	"mul": func(a, b float64) int { return int(a * b) },
 }
